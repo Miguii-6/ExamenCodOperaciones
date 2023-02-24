a8112e59e6c0703a7089260bdc1bb6e6f9157c60
Miguii-6 <mmarinomartinez@danielcastelao.org>
2023-02-24 09:23:37 +0100

    Principio do examen

---
+++ src/OperacionesBasicas.java
@@ -0,0 +1,54 @@
+public class OperacionesBasicas {
+
+        private static float resultado;
+        static final int SUMA=1;
+        static final int RESTA=2;
+        static final int MULTIPLICACION=3;
+        static final int DIVISION=4;
+
+    /**
+     * @param device --> operación a realizar (SUMA, RESTA, MULTIPLICACIÓN, DIVISIÓN)
+     * @param num1 --> primer numero de la operación
+     * @param num2 --> segundo numero de la operación (divisor en caso de division)
+     * @return resultado o null en caso de fallo.
+     */
+
+
+
+
+        public static float operacions(int device, float num1, float num2){
+            switch (device){
+                case SUMA:
+                    try{
+                        resultado=num1+num2;
+                    }catch (Exception e){
+                        System.out.println("Error");
+                    }
+                    break;
+
+                case RESTA:
+                    try{
+                        resultado=num1-num2;
+                    }catch (Exception e){
+                        System.out.println("Error");
+                    }
+                    break;
+                case MULTIPLICACION:
+                    try{
+                        resultado=num1*num2;
+                    }catch (Exception e){
+                        System.out.println("Error");
+                    }
+                    break;
+                case DIVISION:
+                    try{
+                        resultado=num1/num2;
+                    }catch (Exception e){
+                        System.out.println("Error");
+                    }
+                    break;
+            }
+            return resultado;
+        }
+
+}
