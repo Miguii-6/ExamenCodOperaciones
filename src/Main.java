d43aac400fb7505994ce57063da7b8071497da86
Miguii-6 <mmarinomartinez@danielcastelao.org>
2023-02-24 09:37:53 +0100

    Fallo en raiz las raices

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int opcion;
         Float resultado;
-        opcion = EntradaSaida.entradaInt("1 --> Hacer suma\n2 --> Hacer resta\n3 --> Hacer multiplicación\n4 --> Hacer división\n5 --> Saír do programa");
+        opcion = EntradaSaida.entradaInt("1 --> Hacer suma\n2 --> Hacer resta\n3 --> Hacer multiplicación\n4 --> Hacer división\n5 --> Saír do programa\n6 --> Hacer raiz cuadrada\n7 --> Hacer raiz cubica");
         if(opcion > 0 && opcion < 5) {
             resultado = OperacionesBasicas.operacions(opcion, EntradaSaida.entradaFloat("Teclee o primer número da operación"), EntradaSaida.entradaFloat("Teclee o segundo número da operación"));
             if (resultado==null){
